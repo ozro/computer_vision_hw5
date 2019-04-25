af1dc3b7d1dbf1b7452257a8b89babfb49e4980c
Oliver Zhang <oliverzhang1@yahoo.com>
2019-04-25 16:22:56 -0400

    Finished

--- q4.py
+++ q4.py
@@ -11,10 +11,27 @@ import skimage.segmentation
 # takes a color image
 # returns a list of bounding boxes and black_and_white image
 def findLetters(image):
-    bboxes = []
-    bw = None
     # insert processing in here
     # one idea estimate noise -> denoise -> greyscale -> threshold -> morphology -> label -> skip small boxes 
     # this can be 10 to 15 lines of code using skimage functions
-    
+
+    # Denoise
+    image = skimage.restoration.denoise_bilateral(image, multichannel=True)
+    # Greyscale
+    image = skimage.color.rgb2gray(image)
+    # Threshold the image
+    th = skimage.filters.threshold_otsu(image)
+    bw = image < th
+    # Morphology
+    bw = skimage.morphology.closing(bw, skimage.morphology.disk(3))
+    bw = skimage.segmentation.clear_border(bw)
+    # Label
+    label = skimage.morphology.label(bw, connectivity=2)
+    props = skimage.measure.regionprops(label)
+    # Get large boxes
+    median = np.median([x.area for x in props])
+    th = median / 2 # Get boxes that are larger than half the mean size
+    bboxes = np.asarray([prop.bbox for prop in props if prop.area > th])
+    bw = (1-bw).astype(np.float)
+
     return bboxes, bw
--- run_q4.py
+++ run_q4.py
@@ -31,16 +31,73 @@ for img in os.listdir('../images'):
                                 fill=False, edgecolor='red', linewidth=2)
         plt.gca().add_patch(rect)
     plt.show()
+
     # find the rows using..RANSAC, counting, clustering, etc.
+    # Get coordinates of bbox centers as (y, x, h, w)
+    max_h = np.max(bboxes[:,2] - bboxes[:,0])
+    coords = [((bbox[0] + bbox[2])//2, (bbox[1] + bbox[3])//2, (bbox[2] - bbox[0]), (bbox[3] - bbox[1])) for bbox in bboxes]
+    coords = sorted(coords, key=lambda x: x[0]) # Sort by y coordinate
+
+    prev_coord = coords[0]
+    prev_row = 0
+    row = []
+    rows = []
+    rows.append(row)
+    for coord in coords:
+        if(coord[0] - prev_coord[0] > max_h * 0.75): # moving on to new row
+            prev_row += 1
+            prev_coord = coord
+            row = []
+            row.append(coord)
+            rows.append(row)
+        else: #within previous row
+            rows[prev_row].append(coord)
+            rows[prev_row] = sorted(rows[prev_row], key=lambda x: x[1]) # maintain row order in x direction
+
     
     # crop the bounding boxes
     # note.. before you flatten, transpose the image (that's how the dataset is!)
     # consider doing a square crop, and even using np.pad() to get your images looking more like the dataset
-    
+
+    dataset = []
+    min_pad = 10
+
+    for row in rows:
+        data = []
+        for y,x,h,w in row:
+            patch = bw[y-h//2:y+h//2, x-w//2:x+w//2]
+            # Pad the patch to a square, using larger dimension
+            if h>w:
+                pad_y = min_pad
+                pad_x = pad_y + h-w
+            elif h<w:
+                pad_x = min_pad
+                pad_y = pad_x + w-h
+            else:
+                pad_x = 0
+                pad_y = 0
+            patch = np.pad(patch, ((pad_y, pad_y), (pad_x, pad_x)), 'constant', constant_values=(1, 1))
+            patch = skimage.transform.resize(patch, (32, 32))
+            patch = skimage.morphology.erosion(patch, skimage.morphology.disk(2))
+            # plt.imshow(patch)
+            # plt.show()
+            patch = np.transpose(patch)
+            data.append(patch.flatten())
+        dataset.append(data)
+
     # load the weights
     # run the crops through your neural network and print them out
     import pickle
     import string
     letters = np.array([_ for _ in string.ascii_uppercase[:26]] + [str(_) for _ in range(10)])
     params = pickle.load(open('q3_weights.pickle','rb'))
+
+    res = ""
+    for data in dataset:
+        h1 = forward(data, params, "layer1", sigmoid)
+        probs = forward(h1, params, "output", softmax)
+        pred_ind = np.argmax(probs, axis = 1)
+        res += "".join(letters[pred_ind]) + "\n"
+
+    print(res)
     
